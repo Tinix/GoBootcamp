e55f530885cd5f85fdea0209102429abfb6cab60
Daniel Tinivella <dtinivella@gmail.com>
2022-03-30 11:58:58 -0300

    add exercise 10 Discard the file

---
+++ 06-variables/04-assigment/exercises/10-discard-the-file/main.go
@@ -0,0 +1,35 @@
+// Copyright © 2022 Daniel Tinivella
+// Learn Go Programming Course Bootcamp
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more information  : https://tinix.github.io
+// In-person training  : https://www.linkedin.com/in/tinivella/
+// Follow me on twitter: https://twitter.com/tinix
+
+package main
+
+import (
+	"fmt"
+	"path"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Discard The File
+//
+//  1. Print only the directory using `path.Split`
+//
+//  2. Discard the file part
+//
+// RESTRICTION
+//  Use short declaration
+//
+// EXPECTED OUTPUT
+//  secret/
+// ---------------------------------------------------------
+
+func main() {
+
+	dir, _ := path.Split("secret/file.txt")
+
+	fmt.Println("dir:", dir)
+}
